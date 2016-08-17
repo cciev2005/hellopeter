27a617607a974567b27b085e8baacbde4e289da4
Alexey Popov <cciev2005@gmail.com>
2016-08-16 21:33:17 -0700

    First commit

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,26 @@
+package helloPeter;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+    private final String date;
+
+    public Greeting(long id, String content, String date) {
+        this.id = id;
+        this.content = content;
+        this.date = date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
